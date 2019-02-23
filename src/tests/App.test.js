f96e2ab661062cb77fd0f336df8b426ef5cba088
Tiffany Mathers <tiffany.mathers1@gmail.com>
2019-02-23 15:17:53 +0000

    update_dependencies: update dependencies that had vulnerabilities

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SunShineState from '../SunShineState';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SunShineState />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
